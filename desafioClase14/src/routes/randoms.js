9e30a050b0aa2baa21c6b3fc6f3ac100268723d1
Nicomax0279 <92488330+Nicomax0279@users.noreply.github.com>
2022-12-17 07:50:47 -0300

    act

--- desafioClase14/src/routes/randoms.js
+++ desafioClase14/src/routes/randoms.js
@@ -1,14 +1,22 @@
 import  {  Router } from "express";
 import {fork} from "child_process"
+import {fileURLToPath} from "url";
+import path, { dirname } from "path";
+
+
+
+
+
 const randomRouter = Router();
 
-
+const __dirname = dirname(fileURLToPath(import.meta.url)); 
 let ready = false
+ let childFile = path.join(__dirname,"../child/child.js")
 randomRouter.get("/",(req,res)=>{
     let cant = req.query.cant
     //let numeros = generarNumeros(cant)
    
-    let child = fork("./child");
+    let child = fork(childFile);
   
     child.on("message",(childMessage)=>{
         if(childMessage == "listo"){
@@ -18,7 +26,7 @@ randomRouter.get("/",(req,res)=>{
             res.send(childMessage)
            // const num = childMessage
         }
-        console.log(childMessage)
+       // console.log(childMessage)
 
     })
     
